369a5f91a866a9d282e5a99c46775f18090d4dbd
Maxim L <maksim.ldokov.97@mail.ru>
2024-06-06 22:30:33 +0400

    added some states

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price={555} />
+            <PizzaBlock title="Гавайская" price={250} />
+            <PizzaBlock title="Четыре сыра" price={200} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
