ba1618ba1a11fa0f71aa926f1995c3200095f9b3
Maxim L <maksim.ldokov.97@mail.ru>
2024-06-19 22:17:33 +0400

    added main layout and component for single pizza

---
+++ src/components/LogoBlock.jsx
@@ -0,0 +1,15 @@
+import logoSvg from '../assets/img/pizza-logo.svg';
+
+function LogoBlock() {
+  return (
+    <div className="header__logo">
+      <img width="38" src={logoSvg} alt="Pizza logo" />
+      <div>
+        <h1>React Pizza</h1>
+        <p>самая вкусная пицца во вселенной</p>
+      </div>
+    </div>
+  );
+}
+
+export default LogoBlock;
---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+function FullPizza() {
+  const [pizza, setPizza] = React.useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://66641d59932baf9032aa0642.mockapi.io/items/' + id);
+
+        setPizza(data);
+      } catch (error) {
+        alert('Пицца не нашлась');
+        navigate('/');
+      }
+    }
+    fetchPizza();
+  }, [id, navigate]);
+
+  if (!pizza) {
+    return <div>Загрузка пиццы</div>;
+  }
+  return (
+    <div className="container">
+      <img className="pizza-block__image" src={pizza.imageUrl} alt="Пицца" />
+      <h2>{pizza.title}</h2>
+
+      <h4>{pizza.price} ₽</h4>
+    </div>
+  );
+}
+
+export default FullPizza;
